3e81feb4f8ff5d616ca4c56c5d565fe3e543f5ce
sschr15 <steven.sschr15@hotmail.com>
2023-10-04 16:05:01 -0500

    Set up for maven publishing + Apple targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
 plugins {
     kotlin("multiplatform") version "1.9.10"
+    id("org.jetbrains.dokka") version "1.9.0"
     `maven-publish`
+    signing
 }
 
 version = "24.0.1"
@@ -14,7 +18,11 @@ kotlin {
     jvm()
     js {
         nodejs()
+        browser()
     }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasm()
 
     androidNativeArm32()
     androidNativeArm64()
@@ -26,7 +34,27 @@ kotlin {
 
     mingwX64()
 
-    //TODO Apple native targets
+    ios()
+    tvos()
+    watchos()
+    macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    tvosSimulatorArm64()
+    watchosSimulatorArm64()
+}
+
+val dokkaJar = tasks.create<Jar>("dokkaJar") {
+    from(tasks.dokkaHtml)
+    archiveClassifier = "javadoc"
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    if (signingKey != null && signingPassword != null) {
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
 }
 
 publishing {
@@ -44,6 +72,43 @@ publishing {
     } else {
         repositories {
             mavenLocal()
+        }
+    }
+
+    publications {
+        withType<MavenPublication> {
+            artifact(dokkaJar) {
+                classifier = "javadoc"
+            }
+
+            pom {
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+
+                name = "JB Annotations - Multiplatform Port"
+                description = "A port of Jetbrains Java annotations to Kotlin/Multiplatform, " +
+                        "for those sweet annotations everywhere"
+                url = "https://github.com/sschr15/jetbrains-annotations-kmp"
+
+                developers {
+                    developer {
+                        name = "sschr15"
+                        email = "me@sschr15.com"
+                        url = "https://github.com/sschr15"
+                        timezone = "America/Chicago"
+                    }
+                }
+
+                scm {
+                    connection = this@pom.url.get().replace("https", "scm:git:git")
+                    developerConnection = connection.get().replace("git://", "ssh://")
+                    url = this@pom.url
+                }
+            }
         }
     }
 }
